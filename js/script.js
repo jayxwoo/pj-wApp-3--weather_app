bc49f430c81a1bff64f857a1fd1a32252b5c1187
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-10-01 05:50:34 +1300

    // attached an event listener to the form

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ import './default.js';
 // ========== script ==========
 // reference
 const key = '9nmmB1MUGkNFqHusjj7qybPu90aAnj8T';
+const searchForm = document.querySelector('.search-form');
+
+// get city
+
+// get weatther
+
+// main
+const main = function () {
+    searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('submitted');
+    });
+};
+
+main();
